c4b0b0ef0ad6c8423c610d10b9fd1b18fac9dc61
Nick Koole <nickkoole@hotmail.com>
2023-10-24 13:57:50 +0200

    #4 product_id weggehaald op detailpagina conform opdracht

--- product.php
+++ product.php
@@ -14,7 +14,6 @@
         echo '<br><br>';
 
         echo '<img src="' . $data['product']["product_picture_location"] . '" class="productpage" alt="Een foto"><br>' .
-        'Product id: ' . $data['product']["product_id"] . '<br>' .
         'Artikel: ' . $data['product']["name"] . '<br>' .
         'Beschrijving: ' . $data['product']["description"] . '<br>' .
         'Prijs: €' . $data['product']["price"] . '<br>';
