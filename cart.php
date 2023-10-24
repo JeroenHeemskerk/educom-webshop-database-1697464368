b1e3e790c51cddd0de94ee386b845ba1b2d7b3d7
Nick Koole <nickkoole@hotmail.com>
2023-10-24 15:47:38 +0200

    #19

--- cart.php
+++ cart.php
@@ -8,14 +8,42 @@
         echo '<br>';
 
         if (!empty($data['cartLines'])){
-            print_r($data);
             showTable($data);
             showBuyAction('Koop nu!');
         } else {
             echo '<h2>Er is nog niets te tonen nu de winkelmand nog leeg is. U kunt in de webshop ' . 
             'iets toevoegen om aan te schaffen en dit op deze pagina afrekenen.</h2>';
-        }
-        
+        }        
     }
 
+    function showTable($data) {
+
+        tableStart();
+        rowStart();
+        headerCell('Foto:'); 
+        headerCell('Product:');
+        headerCell('Beschrijving:');
+        headerCell('Prijs per stuk:');
+        headerCell('Hoeveelheid:');
+        headerCell('Subtotaal:');
+        rowEnd();
+        
+        foreach ($data['cartLines'] as $product_id => $value){
+            rowStart();
+            dataCell('<img class="tablePicture" src="' . $data['cartLines'][$product_id]['product_picture_location'] . '" alt="Een foto">', $product_id);
+            dataCell($data['cartLines'][$product_id]['name'], $product_id);
+            dataCell($data['cartLines'][$product_id]['description'], $product_id);
+            dataCell('€' . $data['cartLines'][$product_id]['price']);
+            dataCell($data['cartLines'][$product_id]['amount']);
+            dataCell('€'. $data['cartLines'][$product_id]['subTotal']);
+            rowEnd();
+        }
+    
+        rowStart(); 
+        dataCell('', '', 4);
+        dataCell('Totaal:');  
+        dataCell('€' . $data['total']);
+        rowEnd();
+        tableEnd();
+    }
 ?>
--- tables.php
+++ tables.php
@@ -1,47 +1,23 @@
 <?php
 
-function showTable($data) {
-
-    tableStart();
-    echo '<tr>';  
-    headerCell('Foto:'); 
-    headerCell('Product:');
-    headerCell('Beschrijving:');
-    headerCell('Prijs per stuk:');
-    headerCell('Hoeveelheid:');
-    headerCell('Subtotaal:');
-    echo '</tr>';
-    
-    foreach ($data['cartLines'] as $product_id => $value){
-        echo '<tr>';
-        dataCell('<img class="tablePicture" src="' . $data['cartLines'][$product_id]['product_picture_location'] . '" alt="Een foto">', $product_id);
-        dataCell($data['cartLines'][$product_id]['name'], $product_id);
-        dataCell($data['cartLines'][$product_id]['description'], $product_id);
-        dataCell('€' . $data['cartLines'][$product_id]['price']);
-        dataCell($data['cartLines'][$product_id]['amount']);
-        dataCell('€'. $data['cartLines'][$product_id]['subTotal']);
-        echo '</tr>';
-    }
-
-    echo '<tr>' . 
-    '<td colspan="4"></td>';
-    dataCell('Totaal:');  
-    dataCell('€' . $data['total']);
-    echo '</tr>';
-    tableEnd();
-}
-
-function dataCell($value = "", $product_id = "") {
+function dataCell($value = "", $product_id = "", $colspan = 1) {
     if ($product_id != ""){
-        echo '<td><a class="productLink" href="index.php?page=details&product_id=' . $product_id . '"><div class="pagetext">' . $value . '</div></a></td>';
+        echo '<td colspan="' . $colspan . '"><a class="productLink" href="index.php?page=details&product_id=' . $product_id . '"><div class="pagetext">' . $value . '</div></a></td>';
     } else {
-        echo '<td>' . $value . '</td>';
-    }
-    
+        echo '<td colspan="' . $colspan . '">' . $value . '</td>';
+    }    
 }
 
 function headerCell($value) {
     echo '<th>' . $value . '</th>';
+}
+
+function rowStart() {
+    echo '<tr>';
+}
+
+function rowEnd() {
+    echo '</tr>';
 }
 
 function tableStart() {
